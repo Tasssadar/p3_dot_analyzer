e62b04fc0b22525ac809aafcaaef475a122b63b1
Vojtěch Boček <vbocek@gmail.com>
2026-01-29 17:59:36 +0100

    libusb

--- build_exe.ps1
+++ build_exe.ps1
@@ -1,1 +1,1 @@
-uv run pyinstaller -F -y -w --collect-all p3_dot_analyzer --hidden-import dearpygui.dearpygui --hidden-import cv2 .\p3_dot_analyzer_cli.py
+uv run pyinstaller -F -y -w --collect-all p3_dot_analyzer --hidden-import dearpygui.dearpygui --hidden-import cv2 --add-data libusb0.dll:. .\p3_dot_analyzer_cli.py
