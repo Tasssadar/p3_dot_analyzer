3a57f038c68404ec285a8289e943ec7bd28e459e
Vojtěch Boček <vbocek@gmail.com>
2026-01-29 17:16:11 +0100

    feat: GH actions

--- build_exe.ps1
+++ build_exe.ps1
@@ -1,1 +1,1 @@
-uv run pyinstaller -F -y -w --collect-all p3_dot_analyzer --hidden-import dearpygui.dearpygui --hidden-import cv2 .\p3_camera_cli.py
+uv run pyinstaller -F -y -w --collect-all p3_dot_analyzer --hidden-import dearpygui.dearpygui --hidden-import cv2 .\p3_dot_analyzer_cli.py
